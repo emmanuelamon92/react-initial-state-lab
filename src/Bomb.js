49530f96adc199cf78a711f125b080e981b49b2f
Emmanuel <emmanuelamon92@gmail.com>
2021-06-09 13:10:10 -0400

    completed assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,16 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+
+    state = {
+        secondsLeft: this.props.initialCount
+    }
+
+    bomberTimer = (timer) => timer === 0 ? 'Boom!': `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    render() {
+        return this.bomberTimer(this.state.secondsLeft)
+        // return `${this.state.secondsLeft} seconds left before I go boom!`
+    }
+}
